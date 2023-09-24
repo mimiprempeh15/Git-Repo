a350a171ecfb2e8e4c8e4e35e1c181680bcbd5c2
Miriam Bosompim <nana.mb1984@gmail.com>
2023-09-24 18:59:04 +0000

    my first commit

---
+++ Deploy.java
@@ -0,0 +1,6 @@
+#!/bin/bash 
+echo "Welcome to Zelle"
+echo "Zelle your payment gateway"
+echo "Zelle is the gateway of the future"
+echo "Zelle is offering 30% discount"
+echo "Good deals for returning clients"
